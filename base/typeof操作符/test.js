169f68d3cd60af769d4b53211a0e5653cfac3bb1
semyin <semyin@kplocker.com>
2022-03-03 11:28:37 +0800

    typeof 操作符

---
+++ base/typeof操作符/test.js
@@ -0,0 +1,14 @@
+const str = ''
+const num = 1
+const bool = false
+
+let u
+
+function fn() {}
+
+console.log(typeof str) // string
+console.log(typeof num) // number
+console.log(typeof bool) // boolean
+console.log(typeof u) // undefined
+console.log(typeof null) // object
+console.log(typeof fn) // Function
